d4baba3f5c3b02714506a825d6021bcf4f492a00
tomascd123 <tomasd2cuba@gmail.com>
2023-01-31 00:58:46 -0500

    mk_mesh2

--- difuncts.py
+++ difuncts.py
@@ -368,6 +368,50 @@ def mk_mesh(x=5, y=5, xmin=-180, xmax=180, ymin=-180, ymax=180):
             meshgrid.append(xy_pair)
     return meshgrid
 
+def mk_mesh2(xy_min=-180, xy_max=180, xy_step = 60):
+    """
+    Creates a list of X,Y pairs of values in a grid format.
+    RETURN created meshgrid.
+    """
+
+    xy_values = np.arange(xy_min, xy_max + xy_step, xy_step)
+    xy_values_up1_down = []
+    xy_values_up_down_second_fix = []
+    xy_values_down_down = []
+    xy_values_up_down = []
+    for i in xy_values:
+        down_down = (i, i)
+        counter_flow = (i, -1*i)
+        for j in xy_values:
+            fix_one_flow = (i, j)
+            fix_second_flow = (j, -1*i)
+            xy_values_up1_down.append(fix_one_flow)
+            xy_values_up_down_second_fix.append(fix_second_flow)
+        xy_values_down_down.append(down_down)
+        xy_values_up_down.append(counter_flow)
+
+    xy_values_up_down1 = xy_values_up1_down.copy()
+    xy_values_up1_down.reverse()
+    xy_values_up_up = xy_values_down_down.copy()
+    xy_values_down_down.reverse()
+    xy_values_down_up = xy_values_up_down.copy()
+    xy_values_up_down.reverse()
+    xy_values_up_down_second_fix1 = xy_values_up_down_second_fix.copy()
+    xy_values_up_down_second_fix.reverse()
+
+    meshes = {}
+
+    meshes['down-up'] = xy_values_up_down
+    meshes['up-down'] = xy_values_down_up
+    meshes['up-up'] = xy_values_down_down
+    meshes['down-down'] = xy_values_up_up
+    meshes['up*-down'] = xy_values_up_down1
+    meshes['down*-up'] = xy_values_up1_down
+    meshes['up-down*'] = xy_values_up_down_second_fix
+    meshes['down-up*'] = xy_values_up_down_second_fix1
+
+    return meshes
+
 def extract_values(itertools_product, reverse=False):
     if reverse:
         return [(x[1], x[0]) for x in itertools_product]
